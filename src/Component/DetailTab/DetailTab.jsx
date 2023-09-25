3b01bd64322d1fbeb41f57e4697133e43f0f677c
tajkir-alam <tajkir.alam@gmail.com>
2023-09-25 13:10:27 +0600

    tab section done

--- src/Component/DetailTab/DetailTab.jsx
+++ src/Component/DetailTab/DetailTab.jsx
@@ -6,19 +6,19 @@ import './detailTab.css';
 const DetailTab = () => {
     return (
         <section className='detail-tab'>
-            <Tabs>
+            <Tabs className='detail-tabs'>
                 <TabList>
                     <Tab>Details</Tab>
                     <Tab>More Information</Tab>
                 </TabList>
 
                 <TabPanel>
-                    <p>
+                    <p className='tab-panel'>
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
                     </p>
                 </TabPanel>
                 <TabPanel>
-                    <p>
+                    <p className='tab-panel'>
                         <b>Toad</b> (<i>Japanese:=</i>) is a fictional character who primarily
                         appears in Nintendo's Mario franchise. Created by Japanese video game designer
                         he Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et, beatae.
