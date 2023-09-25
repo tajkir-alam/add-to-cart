6f66976aeb40ec2810a75f6235d5229ceeceaa6f
tajkir-alam <tajkir.alam@gmail.com>
2023-09-25 18:03:47 +0600

    button global design added

--- src/Component/Product/RelatedItem.jsx
+++ src/Component/Product/RelatedItem.jsx
@@ -12,7 +12,7 @@ const RelatedItem = ({ url, title, weight, price }) => {
                 <p className='related-items-content-price'>{price}</p>
             </div>
 
-            <div className='related-items-cart-icon flex items-center'>
+            <div className='related-items-cart-icon flex items-center cursor-pointer'>
                 <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 12 12" fill="none">
                     <path fillRule="evenodd" clipRule="evenodd" d="M10.8 4.8H7.2V1.2C7.2 0.54 6.66 0 6 0C5.34 0 4.8 0.54 4.8 1.2V4.8H1.2C0.54 4.8 0 5.34 0 6C0 6.66 0.54 7.2 1.2 7.2H4.8V10.8C4.8 11.46 5.34 12 6 12C6.66 12 7.2 11.46 7.2 10.8V7.2H10.8C11.46 7.2 12 6.66 12 6C12 5.34 11.46 4.8 10.8 4.8Z" fill="#F3F3EF" />
                 </svg>
--- src/Component/RelatedProducts/RelatedProductCard.jsx
+++ src/Component/RelatedProducts/RelatedProductCard.jsx
@@ -8,23 +8,23 @@ const RelatedProductCard = ({ product }) => {
         <div className='card-container'>
             <div className='quantity-management'>
                 <div className="quantity-container">
-                    <button className='quantity-btn flex items-center'>
+                    <button className='quantity-btn flex items-center cursor-pointer'>
                         <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" viewBox="0 0 36 36" fill="none">
                             <path d="M18 9C18.2984 9 18.5845 9.11853 18.7955 9.32951C19.0065 9.54048 19.125 9.82663 19.125 10.125V16.875H25.875C26.1734 16.875 26.4595 16.9935 26.6705 17.2045C26.8815 17.4155 27 17.7016 27 18C27 18.2984 26.8815 18.5845 26.6705 18.7955C26.4595 19.0065 26.1734 19.125 25.875 19.125H19.125V25.875C19.125 26.1734 19.0065 26.4595 18.7955 26.6705C18.5845 26.8815 18.2984 27 18 27C17.7016 27 17.4155 26.8815 17.2045 26.6705C16.9935 26.4595 16.875 26.1734 16.875 25.875V19.125H10.125C9.82663 19.125 9.54048 19.0065 9.32951 18.7955C9.11853 18.5845 9 18.2984 9 18C9 17.7016 9.11853 17.4155 9.32951 17.2045C9.54048 16.9935 9.82663 16.875 10.125 16.875H16.875V10.125C16.875 9.82663 16.9935 9.54048 17.2045 9.32951C17.4155 9.11853 17.7016 9 18 9Z" fill="white" />
                         </svg>
                     </button>
                     <div className='quantity-adjustment flex items-center'>
-                        <span className="quantity-decrease">
+                        <button className="quantity-decrease cursor-pointer">
                             <svg xmlns="http://www.w3.org/2000/svg" width="37" height="36" viewBox="0 0 37 36" fill="none">
                                 <path d="M8.09998 16.5H29.1V19.5H8.09998V16.5Z" fill="white" />
                             </svg>
-                        </span>
+                        </button>
                         <p>{quantity}</p>
-                        <span className="quantity-increase">
+                        <button className="quantity-increase cursor-pointer">
                             <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" viewBox="0 0 36 36" fill="none">
                                 <path d="M18 9C18.2984 9 18.5845 9.11853 18.7955 9.32951C19.0065 9.54048 19.125 9.82663 19.125 10.125V16.875H25.875C26.1734 16.875 26.4595 16.9935 26.6705 17.2045C26.8815 17.4155 27 17.7016 27 18C27 18.2984 26.8815 18.5845 26.6705 18.7955C26.4595 19.0065 26.1734 19.125 25.875 19.125H19.125V25.875C19.125 26.1734 19.0065 26.4595 18.7955 26.6705C18.5845 26.8815 18.2984 27 18 27C17.7016 27 17.4155 26.8815 17.2045 26.6705C16.9935 26.4595 16.875 26.1734 16.875 25.875V19.125H10.125C9.82663 19.125 9.54048 19.0065 9.32951 18.7955C9.11853 18.5845 9 18.2984 9 18C9 17.7016 9.11853 17.4155 9.32951 17.2045C9.54048 16.9935 9.82663 16.875 10.125 16.875H16.875V10.125C16.875 9.82663 16.9935 9.54048 17.2045 9.32951C17.4155 9.11853 17.7016 9 18 9Z" fill="white" />
                             </svg>
-                        </span>
+                        </button>
                     </div>
                 </div>
             </div>
