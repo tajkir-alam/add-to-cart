7b70f360b96479d47176b3e59d61b24dceb77f4e
tajkir-alam <tajkir.alam@gmail.com>
2023-09-26 14:27:57 +0600

    detail tab CSS update by manually

--- src/Component/DetailTab/DetailTab.jsx
+++ src/Component/DetailTab/DetailTab.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Tab, TabList, TabPanel, Tabs } from 'react-tabs';
-import 'react-tabs/style/react-tabs.css';
 import './detailTab.css';
 
 const DetailTab = () => {
